f2aa9f1c535830a855bc9dce74f08105eabba038
two point six <mcjulcz@tutanota.com>
2021-06-17 02:09:27 +0200

    Removed C Code

--- c-code/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-  FILE *fp;
-  fp = fopen("config/args", "w");
-  for(unsigned i=0; i < argc; ++i)
-        fprintf(fp, "%s", argv[i]);
-  fclose(fp);
-}
